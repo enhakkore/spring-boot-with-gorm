2644d1936a5a594f782de0e98746632a92580efa
enhakkore <ahn.ephraim@gmail.com>
2019-11-26 15:59:09 +0900

    Update ReadingListApplication.groovy

--- src/main/groovy/readinglist/ReadingListApplication.groovy
+++ src/main/groovy/readinglist/ReadingListApplication.groovy
@@ -2,12 +2,25 @@ package readinglist
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.method.support.HandlerMethodArgumnetResolver
+import org.springframework.web.servlet.config.annotaion.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
-class ReadingListApplication {
+class ReadingListApplication extends WebMvcConfigurerAdapter {
 
 	static void main(String[] args) {
 		SpringApplication.run(ReadingListApplication, args)
+	}
+
+	@Override
+	void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewControllers("/login").setViewName("login")
+	}
+
+	@Override
+	void addArgumentResolvers(List<HandlerMethodArgumnetResolver> argumentResolvers) {
+		argumentResolvers.add(new ReaderHandlerMethodArgumentResolver())
 	}
 
 }
