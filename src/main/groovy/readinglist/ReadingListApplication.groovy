6e7f3e4d2be888b50209a017cd4b56abd90381d0
enhakkore <ahn.ephraim@gmail.com>
2019-11-26 14:05:33 +0900

    Create project

---
+++ src/main/groovy/readinglist/ReadingListApplication.groovy
@@ -0,0 +1,13 @@
+package readinglist
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ReadingListApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ReadingListApplication, args)
+	}
+
+}
